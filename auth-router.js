f0fa3bcf5abc9374e8aaf6e59e444cea9dfa4a91
Umesh29082003 <121432618+Umesh29082003@users.noreply.github.com>
2024-03-22 14:43:11 +0530

    Update auth-router.js

--- auth-router.js
+++ auth-router.js
@@ -1,9 +1,11 @@
+//THIS FILE CONTAINS THE ROUTERS
+
 const express=require('express')
 
 const router=express.Router()
 const auth=require("./auth-Controller")
 
-router.route('/signup').post(auth.signup)
-router.route('/login').post(auth.login)
+router.route('/signup').post(auth.signup)  //route to signup page //Validation to be added
+router.route('/login').post(auth.login)  //route to handle login page
 
 module.exports=router
