e39ed4ed00ef67fd293247152509c9fd7c7e3d29
Umesh29082003 <121432618+Umesh29082003@users.noreply.github.com>
2024-03-23 16:42:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+//THIS IS THE SERVER
+
+const express=require('express')
+const app=express()
+const connectDb=require('./connect-db')
+const router=require('./User/router')
+app.use(express.json())
+
+
+//Connect to DB
+connectDb().then(()=>{
+    console.log("Hello DB")
+})
+
+//Use the Routers
+app.use('/',router)
+
+//Start Server
+const PORT =3000;
+app.listen(PORT, ()=>{
+    console.log('server is running at port:'+PORT);
+})
